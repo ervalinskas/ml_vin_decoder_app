c7972f3d3cb745c75384152a084939a2b3a8653c
Erikas Valinskas <erikasa32@gmail.com>
2024-02-29 08:14:29 +0200

    Remove config variable to avoid linting errors

--- vin_decoder/cli.py
+++ vin_decoder/cli.py
@@ -5,7 +5,7 @@ from vin_decoder.config import AppConfig
 @click.group()
 @click.option("--config_file_path", envvar="CONFIG_FILE_PATH")
 def vin_decoder(config_file_path):
-    config = AppConfig.from_toml(path=config_file_path)
+    AppConfig.from_toml(path=config_file_path)
 
 
 @vin_decoder.command()
